145b8076839adbce95a9f1aae8b6d864828693ed
David Bryant <david@ip0af5216a.int.uvm.edu>
2019-03-18 14:08:58 -0400

    hex gutter

--- consts.js
+++ consts.js
@@ -3,7 +3,7 @@ function preload() {
 }
 
 let zoom = innerHeight / 50;
-let gutter = 0.3;
+let gutter = 0.1;
 let hexRadius = 6;
 
 function edgeRadius(radius) {
@@ -16,97 +16,97 @@ const board = {
   hexes: [
     {
       aoff: (4 * Math.PI) / 3,
-      roff: 2,
+      roff: 2 + 2 * gutter,
       vertices: [2, 3, 11, 10, 9, 1]
     },
     {
       aoff: (3 * Math.PI) / 2,
-      roff: edgeRadius(2),
+      roff: edgeRadius(2) + edgeRadius(2) * gutter,
       vertices: [4, 5, 13, 12, 11, 3]
     },
     {
       aoff: (5 * Math.PI) / 3,
-      roff: 2,
+      roff: 2 + 2 * gutter,
       vertices: [6, 7, 15, 14, 13, 5]
     },
     {
       aoff: (7 * Math.PI) / 6,
-      roff: edgeRadius(2),
+      roff: edgeRadius(2) + edgeRadius(2) * gutter,
       vertices: [9, 10, 20, 19, 18, 8]
     },
     {
       aoff: (4 * Math.PI) / 3,
-      roff: 1,
+      roff: 1 + 1 * gutter,
       vertices: [11, 12, 22, 21, 20, 10]
     },
     {
       aoff: (5 * Math.PI) / 3,
-      roff: 1,
+      roff: 1 + 1 * gutter,
       vertices: [13, 14, 24, 23, 22, 12]
     },
     {
       aoff: (11 * Math.PI) / 6,
-      roff: edgeRadius(2),
+      roff: edgeRadius(2) + edgeRadius(2) * gutter,
       vertices: [15, 16, 26, 25, 24, 14]
     },
     {
       aoff: Math.PI,
-      roff: 2,
+      roff: 2 + 2 * gutter,
       vertices: [18, 19, 30, 29, 28, 17]
     },
     {
       aoff: Math.PI,
-      roff: 1,
+      roff: 1 + 1 * gutter,
       vertices: [20, 21, 32, 31, 30, 19]
     },
     {
       aoff: 0,
-      roff: 0,
+      roff: 0 + 0 * gutter,
       vertices: [22, 23, 34, 33, 32, 21]
     },
     {
       aoff: 0,
-      roff: 1,
+      roff: 1 + 1 * gutter,
       vertices: [24, 25, 36, 35, 34, 23]
     },
     {
       aoff: 0,
-      roff: 2,
+      roff: 2 + 2 * gutter,
       vertices: [26, 27, 38, 37, 36, 25]
     },
     {
       aoff: (5 * Math.PI) / 6,
-      roff: edgeRadius(2),
+      roff: edgeRadius(2) + edgeRadius(2) * gutter,
       vertices: [30, 31, 41, 40, 39, 29]
     },
     {
       aoff: (2 * Math.PI) / 3,
-      roff: 1,
+      roff: 1 + 1 * gutter,
       vertices: [32, 33, 43, 42, 41, 31]
     },
     {
       aoff: Math.PI / 3,
-      roff: 1,
+      roff: 1 + 1 * gutter,
       vertices: [34, 35, 45, 44, 43, 33]
     },
     {
       aoff: Math.PI / 6,
-      roff: edgeRadius(2),
+      roff: edgeRadius(2) + edgeRadius(2) * gutter,
       vertices: [36, 37, 47, 46, 45, 35]
     },
     {
       aoff: (2 * Math.PI) / 3,
-      roff: 2,
+      roff: 2 + 2 * gutter,
       vertices: [41, 42, 50, 49, 48, 40]
     },
     {
       aoff: Math.PI / 2,
-      roff: edgeRadius(2),
+      roff: edgeRadius(2) + edgeRadius(2) * gutter,
       vertices: [43, 44, 52, 51, 50, 42]
     },
     {
       aoff: Math.PI / 3,
-      roff: 2,
+      roff: 2 + 2 * gutter,
       vertices: [45, 46, 54, 53, 52, 44]
     }
   ]
@@ -119,6 +119,9 @@ board.hexes.forEach((hex, hi) => {
   hex.y = function() {
     return sin(this.aoff) * this.roff * edgeRadius(hexRadius) * 2;
   };
+  hex.pos = function() {
+    return createVector(hex.x(), hex.y());
+  }
   // subdivide into 6 triangles
   // for each triangle, subdivide twice
   hex.surface = [
@@ -267,28 +270,38 @@ function subdivide(hex, divisions, x, y, angle, radius, division) {
 
 const colors = {};
 function setColors() {
-  colors.desert = color(255, 238, 160);
-  colors.wood = color(87, 126, 108);
-  colors.brick = color(198, 99, 79);
-  colors.wheat = color(255, 222, 67);
-  colors.sheep = color(144, 207, 124);
-  colors.ore = color(107, 96, 124);
+  colors.desert = {color: color(255, 238, 160)}
+  colors.wood = {color: color(87, 126, 108)}
+  colors.brick = {color: color(198, 99, 79)}
+  colors.wheat = {color: color(255, 222, 67)}
+  colors.sheep = {color: color(144, 207, 124)}
+  colors.ore = {color: color(107, 96, 124)}
 
-  colors.desertDark = color(162, 148, 90);
-  colors.woodDark = color(63, 83, 78);
-  colors.brickDark = color(119, 61, 61);
-  colors.wheatDark = color(162, 135, 78);
-  colors.sheepDark = color(98, 128, 97);
-  colors.oreDark = color(70, 63, 82);
+  colors.desertDark = {color: color(162, 148, 90)}
+  colors.woodDark = {color: color(63, 83, 78)}
+  colors.brickDark = {color: color(119, 61, 61)}
+  colors.wheatDark = {color: color(162, 135, 78)}
+  colors.sheepDark = {color: color(98, 128, 97)}
+  colors.oreDark = {color: color(70, 63, 82)}
 
-  colors.woodLight = color(124, 181, 155);
-  colors.brickLight = color(244, 160, 144);
-  colors.oreLight = color(154, 135, 183);
+  colors.woodLight = {color: color(124, 181, 155)}
+  colors.brickLight = {color: color(244, 160, 144)}
+  colors.oreLight = {color: color(154, 135, 183)}
 
-  colors.player1 = color(224, 26, 26);
-  colors.player2 = color(247, 151, 8);
-  colors.player3 = color(255, 255, 255);
-  colors.player4 = color(31, 84, 255);
+  colors.player1 = {color: color(224, 26, 26)}
+  colors.player2 = {color: color(247, 151, 8)}
+  colors.player3 = {color: color(255, 255, 255)}
+  colors.player4 = {color: color(31, 84, 255)}
+
+  
+  for (const colorKey of Object.keys(colors)) {
+    colors[colorKey].h = hue(colors[colorKey].color);
+    colors[colorKey].s = saturation(colors[colorKey].color);
+    colors[colorKey].v = brightness(colors[colorKey].color);
+    colors[colorKey].r = red(colors[colorKey].color)
+    colors[colorKey].g = green(colors[colorKey].color)
+    colors[colorKey].b = blue(colors[colorKey].color)
+  }
 }
 
 const resources = [];
--- sketch.js
+++ sketch.js
@@ -18,39 +18,44 @@ function setup() {
 
 // ---------------------------
 
+let rotationX = Math.PI/4;
 let rotationZ = 0;
 
 function draw() {
   background(0);
 
-  translate(0, -zoom * 5, 0);
-
   scale(zoom);
 
-  rotateX(PI / 3);
+  translate(0, -7, 0);
+  rotateX(rotationX);
   rotateZ(rotationZ);
+  // directionalLight(255, 255, 255, 0, 0, 0.5);
+  // pointLight(255, 255, 255, 10, 10, 10);
 
-  ambientLight(255);
-  ambientMaterial(255);
-  sphere(0);
+  // ambientLight(255);
+  // ambientMaterial(255);
+  // sphere(0);
 
-  pointLight(255, 255, 255, 0, 0, 0);
+  // pointLight(255, 255, 255, 0, 0, 0);
+
+  // pointLight(255, 255, 255, 10, 10, 10);
 
   board.hexes.forEach(hex => {
     const c = colors[hex.resource];
-    ambientMaterial(c);
+    // fill(
+    //   `hsb(${parseInt(c.h)}, ${parseInt(c.s)}%, ${parseInt(c.v) *
+    //     random(0.9, 1.1)}%)`
+    // );
+    fill(c.color);
     noStroke();
-
-    // stroke(255, 20);
-
-    const gaussian = makeGaussian(5, 0, 0, 3, 1);
 
     function subdivide(hex, divisions, x, y, angle, radius, division) {
       if (!division) division = 1;
 
       if (division < divisions) {
         subdivide(hex, divisions, x, y, angle, radius / 2, division + 1);
-        subdivide(hex,
+        subdivide(
+          hex,
           divisions,
           x + (cos(angle + (2 * PI) / 3) * radius) / 2,
           y + (sin(angle + (2 * PI) / 3) * radius) / 2,
@@ -58,7 +63,8 @@ function draw() {
           radius / 2,
           division + 1
         );
-        subdivide(hex, 
+        subdivide(
+          hex,
           divisions,
           x + (cos(angle + PI / 3) * radius) / 2,
           y + (sin(angle + PI / 3) * radius) / 2,
@@ -66,7 +72,8 @@ function draw() {
           radius / 2,
           division + 1
         );
-        subdivide(hex,
+        subdivide(
+          hex,
           divisions,
           x +
             (cos(angle + (2 * PI) / 3) * radius) / 2 +
@@ -81,97 +88,186 @@ function draw() {
         return;
       }
 
-      let vertx, verty;
-      function vertz() {
-        return gaussian(sqrt(pow(vertx - hex.x(), 2) + pow(verty - hex.y(), 2)), 0) * noise(vertx, verty);
-        // return sqrt(pow(vertx - hex.x(), 2) + pow(verty - hex.y(), 2));
+      let vert1, vert2, vert3;
+      function vertavg() {
+        return createVector(
+          (vert1.x + vert2.x + vert3.x) / 3,
+          (vert1.y, vert2.y, vert3.y) / 3
+        );
+      }
+      function vertz(vert) {
+        let gaussian = makeGaussian(5, 0, 0, 3, 1);
+        
+        // console.log(p5.Vector.dist(hex.pos(), vertavg()));
+        // console.log(vertavg().dist(hex.pos()))
+        // console.log(dist(hex.pos().x, hex.pos().y, vertavg().x, vertavg().y))
+        // console.log(hex.x())
+        
+        switch (hex.resource) {
+          //         colors.desert = {color: color(255, 238, 160)}
+          // colors.wood = {color: color(87, 126, 108)}
+          // colors.brick = {color: color(198, 99, 79)}
+          // colors.wheat = {color: color(255, 222, 67)}
+          // colors.sheep = {color: color(144, 207, 124)}
+          // colors.ore = {color: color(107, 96, 124)}
+          case "desert":
+            gaussian = makeGaussian(3, 0, 0, 3, 1);
+            return (
+              gaussian(hex.pos().dist(vert), 0) *
+              noise(100 + vert.x * 0.5, 100 + vert.y * 0.5)
+            );
+          case "wood":
+            gaussian = makeGaussian(2, 0, 0, 3, 2);
+            return (
+              gaussian(hex.pos().dist(vert), 0) *
+              noise(100 + vert.x, 100 + vert.y) *
+              2
+            );
+          case "brick":
+            gaussian = makeGaussian(3, 0, 0, 4, 2);
+            return (
+              gaussian(hex.pos().dist(vert), 0) *
+              noise(100 + vert.x * 0.1, 100 + vert.y * 0.1)
+            );
+          case "wheat":
+            gaussian = makeGaussian(3, 0, 0, 4, 2);
+            return gaussian(hex.pos().dist(vert), 0) - 0.5;
+          case "sheep":
+            gaussian = makeGaussian(3, 0, 0, 4, 2);
+            return gaussian(hex.pos().dist(vert), 0) - 0.5;
+          case "ore":
+            gaussian = makeGaussian(10, 0, 0, 3, 2);
+            return (
+              gaussian(hex.pos().dist(vert), 0) *
+                noise(100 + vert.x, 100 + vert.y) -
+              0.25
+            );
+          default:
+            return gaussian(hex.pos().dist(vert), 1);
+        }
+      }
+      function radialFill(vert) {
+        // stroke(255)
+        // strokeWeight(3)
+        // point(vert.x, vert.y, 4);
+        // fill(
+        //   `hsb(${parseInt(c.h)}, ${parseInt(c.s)}%, ${c.v  + 10 - abs(hex.pos().dist(vert)) * 2}%)`
+        // );
       }
 
-      beginShape();
-      vertx = x;
-      verty = y;
-      vertex(vertx, verty, vertz());
-      vertx = x + (cos(angle + (2 * PI) / 3) * radius) / 2;
-      verty = y + (sin(angle + (2 * PI) / 3) * radius) / 2;
-      vertex(vertx, verty, vertz());
-      vertx = x + (cos(angle + PI / 3) * radius) / 2;
-      verty = y + (sin(angle + PI / 3) * radius) / 2;
-      vertex(vertx, verty, vertz());
-      endShape();
+      vert1 = createVector(x, y);
+      vert2 = createVector(
+        x + (cos(angle + (2 * PI) / 3) * radius) / 2,
+        y + (sin(angle + (2 * PI) / 3) * radius) / 2
+      );
+      vert3 = createVector(
+        x + (cos(angle + PI / 3) * radius) / 2,
+        y + (sin(angle + PI / 3) * radius) / 2
+      );
+      radialFill(vertavg())
+      beginShape(TRIANGLES);
+      vertex(vert1.x, vert1.y, vertz(vert1));
+      vertex(vert2.x, vert2.y, vertz(vert2));
+      vertex(vert3.x, vert3.y, vertz(vert3));
+      endShape(CLOSE);
 
-      beginShape();
-      vertx = x + (cos(angle + (2 * PI) / 3) * radius) / 2;
-      verty = y + (sin(angle + (2 * PI) / 3) * radius) / 2;
-      vertex(vertx, verty, vertz());
-      vertx = x + cos(angle + (2 * PI) / 3) * radius;
-      verty = y + sin(angle + (2 * PI) / 3) * radius;
-      vertex(vertx, verty, vertz());
-      vertx =
+      // fill(
+      //   `hsb(${parseInt(c.h)}, ${parseInt(c.s)}%, ${parseInt(c.v) *
+      //     random(0.9, 1.1)}%)`
+      // );
+      vert1 = createVector(
+        x + (cos(angle + (2 * PI) / 3) * radius) / 2,
+        y + (sin(angle + (2 * PI) / 3) * radius) / 2
+      );
+      vert2 = createVector(
+        x + cos(angle + (2 * PI) / 3) * radius,
+        y + sin(angle + (2 * PI) / 3) * radius
+      );
+      vert3 = createVector(
         x +
-        (cos(angle + (2 * PI) / 3) * radius) / 2 +
-        (cos(angle + PI / 3) * radius) / 2;
-      verty =
+          (cos(angle + (2 * PI) / 3) * radius) / 2 +
+          (cos(angle + PI / 3) * radius) / 2,
         y +
-        (sin(angle + (2 * PI) / 3) * radius) / 2 +
-        (sin(angle + PI / 3) * radius) / 2;
-      vertex(vertx, verty, vertz());
-      endShape();
+          (sin(angle + (2 * PI) / 3) * radius) / 2 +
+          (sin(angle + PI / 3) * radius) / 2
+      );
+      radialFill(vertavg());
+      beginShape(TRIANGLES);
+      vertex(vert1.x, vert1.y, vertz(vert1));
+      vertex(vert2.x, vert2.y, vertz(vert2));
+      vertex(vert3.x, vert3.y, vertz(vert3));
+      endShape(CLOSE);
 
-      beginShape();
-      vertx = x + (cos(angle + (2 * PI) / 3) * radius) / 2;
-      verty = y + (sin(angle + (2 * PI) / 3) * radius) / 2;
-      vertex(vertx, verty, vertz());
-      vertx =
+      // fill(
+      //   `hsb(${parseInt(c.h)}, ${parseInt(c.s)}%, ${parseInt(c.v) *
+      //     random(0.9, 1.1)}%)`
+      // );
+      vert1 = createVector(
+        x + (cos(angle + (2 * PI) / 3) * radius) / 2,
+        y + (sin(angle + (2 * PI) / 3) * radius) / 2
+      );
+      vert2 = createVector(
         x +
-        (cos(angle + (2 * PI) / 3) * radius) / 2 +
-        (cos(angle + PI / 3) * radius) / 2;
-      verty =
+          (cos(angle + (2 * PI) / 3) * radius) / 2 +
+          (cos(angle + PI / 3) * radius) / 2,
         y +
-        (sin(angle + (2 * PI) / 3) * radius) / 2 +
-        (sin(angle + PI / 3) * radius) / 2;
-      vertex(vertx, verty, vertz());
-      vertx = x + (cos(angle + PI / 3) * radius) / 2;
-      verty = y + (sin(angle + PI / 3) * radius) / 2;
-      vertex(vertx, verty, vertz());
-      endShape();
+          (sin(angle + (2 * PI) / 3) * radius) / 2 +
+          (sin(angle + PI / 3) * radius) / 2
+      );
+      vert3 = createVector(
+        x + (cos(angle + PI / 3) * radius) / 2,
+        y + (sin(angle + PI / 3) * radius) / 2
+      );
+      radialFill(vertavg());
+      beginShape(TRIANGLES);
+      vertex(vert1.x, vert1.y, vertz(vert1));
+      vertex(vert2.x, vert2.y, vertz(vert2));
+      vertex(vert3.x, vert3.y, vertz(vert3));
+      endShape(CLOSE);
 
-      beginShape();
-      vertx =
+      // fill(
+      //   `hsb(${parseInt(c.h)}, ${parseInt(c.s)}%, ${parseInt(c.v) *
+      //     random(0.9, 1.1)}%)`
+      // );
+      vert1 = createVector(
         x +
-        (cos(angle + (2 * PI) / 3) * radius) / 2 +
-        (cos(angle + PI / 3) * radius) / 2;
-      verty =
+          (cos(angle + (2 * PI) / 3) * radius) / 2 +
+          (cos(angle + PI / 3) * radius) / 2,
         y +
-        (sin(angle + (2 * PI) / 3) * radius) / 2 +
-        (sin(angle + PI / 3) * radius) / 2;
-      vertex(vertx, verty, vertz());
-      vertx = x + (cos(angle + PI / 3) * radius) / 2;
-      verty = y + (sin(angle + PI / 3) * radius) / 2;
-      vertex(vertx, verty, vertz());
-      vertx = x + cos(angle + PI / 3) * radius;
-      verty = y + sin(angle + PI / 3) * radius;
-      vertex(vertx, verty, vertz());
-      endShape();
+          (sin(angle + (2 * PI) / 3) * radius) / 2 +
+          (sin(angle + PI / 3) * radius) / 2
+      );
+      vert2 = createVector(
+        x + (cos(angle + PI / 3) * radius) / 2,
+        y + (sin(angle + PI / 3) * radius) / 2
+      );
+      vert3 = createVector(
+        x + cos(angle + PI / 3) * radius,
+        y + sin(angle + PI / 3) * radius
+      );
+      radialFill(vertavg());
+      beginShape(TRIANGLES);
+      vertex(vert1.x, vert1.y, vertz(vert1));
+      vertex(vert2.x, vert2.y, vertz(vert2));
+      vertex(vert3.x, vert3.y, vertz(vert3));
+      endShape(CLOSE);
     }
 
     for (let a = 0; a < TWO_PI; a += PI / 3) {
-      subdivide(hex, 3, hex.x(), hex.y(), a + PI / 2, hexRadius);
+      subdivide(hex, 2, hex.x(), hex.y(), a + PI / 2, hexRadius);
     }
   });
 }
 
-// function keyPressed() {
-//   if (key === "ArrowLeft") {
-//     rotationZ -= PI/12
-//   }
-//   if (key === "ArrowRight") {
-//     rotationZ += PI/12
-//   }
-//   redraw();
-// }
-
 function inputLoop() {
   setTimeout(() => {
+    if (keyIsDown(DOWN_ARROW)) {
+      rotationX -= PI / 24;
+      redraw();
+    } else if (keyIsDown(UP_ARROW)) {
+      rotationX += PI / 24;
+      redraw();
+    }
     if (keyIsDown(LEFT_ARROW)) {
       rotationZ -= PI / 24;
       redraw();
